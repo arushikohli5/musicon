b9b148c9a6da9d00ba45402dc3c16a8829151aa9
arushi <arushi.1467.cse18@chitkara.edu.in>
2022-01-31 21:15:09 +0530

    categories slider complete slick slider

--- js/main.js
+++ js/main.js
@@ -1,13 +1,55 @@
 /*jslint browser: true*/
 /*global $, document, window,Typed*/
 $(document).ready(function(){
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,  
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        } 
-    });  
+
+    const slider = $('.slick-slider');
+      slider.on('wheel', (function(e) {
+        e.preventDefault();
+      
+        if (e.originalEvent.deltaY < 0) {
+          $(this).slick('slickNext');
+        } else {
+          $(this).slick('slickPrev');
+        }
+      }));
+    $('.slick-slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 150,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+
+      
+      
       
 });
     "use strict"
